50223429ceaeab6eaa190b90ef5fd845b027662b
kalczugag <kalczugag@gmail.com>
2024-07-03 13:58:33 +0200

    implementing sortbar

--- frontend/src/components/SortBar/index.tsx
+++ frontend/src/components/SortBar/index.tsx
@@ -1,5 +1,16 @@
+import { ExpandMore } from "@mui/icons-material";
+
 const SortBar = () => {
-    return <div className="flex flex-row justify-between items-center"></div>;
+    return (
+        <div className="flex flex-row justify-between items-center mb-8">
+            <h1 className="text-2xl font-bold">Product</h1>
+            <div>
+                <button className="flex items-center">
+                    Sort <ExpandMore />
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default SortBar;
