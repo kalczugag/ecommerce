31b10a19ef9d66e530c03f391d4f31d31249ebc3
kalczugag <kalczugag@gmail.com>
2024-07-22 13:25:56 +0200

    admin panel initial commit

---
+++ admin/src/App.tsx
@@ -0,0 +1,7 @@
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
+    return <Routes></Routes>;
+};
+
+export default App;
---
+++ admin/src/main.tsx
@@ -0,0 +1,14 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/index.ts";
+import App from "./App.tsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+);
