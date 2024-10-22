ffe6aaa79110fe15770036f35cb7643256b554ca
kalczugag <kalczugag@gmail.com>
2024-10-22 15:39:47 +0200

    update cors configuration

--- backend/app.ts
+++ backend/app.ts
@@ -15,23 +15,8 @@ const app = express();
 
 app.use(
     cors({
-        origin: (origin, callback) => {
-            const allowedOrigins = [
-                "https://admin-ecommerce-df8fb1.netlify.app",
-                "https://frontend-ecommerce-df8fb1.netlify.app",
-                "http://localhost:3000",
-                "http://localhost:5173",
-                "http://localhost:4173",
-            ];
-
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                callback(new Error("Not allowed by CORS"));
-            }
-        },
+        origin: true,
         credentials: true,
-        methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     })
 );
 app.options("*", cors());
@@ -47,23 +32,11 @@ app.post(
 
 app.use(bodyParser.json());
 
-app.use("/api/v1", appRouter());
+app.use("/api/v1", cors(), appRouter());
 
 app.post("/trigger-summary-cron", (req, res) => {
     summaryCronJob();
     res.status(200).send("Summary cron job triggered");
 });
-
-// if (process.env.NODE_ENV === "production") {
-//     const clientBuildPath = path.join(__dirname, "../../frontend/build");
-
-//     app.use(
-//         express.static(clientBuildPath, { maxAge: 30 * 24 * 60 * 60 * 1000 })
-//     );
-
-//     app.get("*", (req, res) => {
-//         res.sendFile(path.join(clientBuildPath, "index.html"));
-//     });
-// }
 
 export default app;
