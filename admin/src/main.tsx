109c5558319d5802784e7ed6af06bf637d8760fc
kalczugag <kalczugag@gmail.com>
2024-11-08 14:17:53 +0100

    remove strictMode

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -5,16 +5,13 @@ import { SnackbarProvider } from "notistack";
 import { store } from "@/store/index.ts";
 import App from "@/App.tsx";
 import "@/index.css";
-import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <SnackbarProvider autoHideDuration={2000} preventDuplicate>
-                    <App />
-                </SnackbarProvider>
-            </BrowserRouter>
-        </Provider>
-    </StrictMode>
+    <Provider store={store}>
+        <BrowserRouter>
+            <SnackbarProvider autoHideDuration={2000} preventDuplicate>
+                <App />
+            </SnackbarProvider>
+        </BrowserRouter>
+    </Provider>
 );
