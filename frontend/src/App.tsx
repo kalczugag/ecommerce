d6efb1ec94223735db4eaafbbb5514fb053a440b
kalczugag <kalczugag@gmail.com>
2025-02-09 13:10:15 +0100

    import react-slick styles globally

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
 import "@/styles/app.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import { Route, Routes } from "react-router-dom";
 import PrivateOutlet from "@/pages/Outlets/PrivateOutlet";
