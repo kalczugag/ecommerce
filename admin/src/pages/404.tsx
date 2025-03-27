a4ccb86775312018672a2c3582f04ae4fd565704
kalczugag <kalczugag@gmail.com>
2025-03-27 19:56:17 +0100

    refactor page not found design

--- admin/src/pages/404.tsx
+++ admin/src/pages/404.tsx
@@ -1,12 +1,33 @@
+import { Link, useLocation } from "react-router-dom";
 import { useTitle } from "@/hooks/useTitle";
 import DefaultLayout from "@/layouts/DefaultLayout";
 
 const NotFound = () => {
     useTitle("Page not found");
 
+    const { pathname } = useLocation();
+
     return (
-        <DefaultLayout>
-            <h1>Page not found</h1>
+        <DefaultLayout className="flex justify-center items-center">
+            <div className="flex flex-col items-start">
+                <Link to="/" className="-ml-3">
+                    <img
+                        src="/icons/logo.svg"
+                        alt="logo"
+                        style={{ height: "120px" }}
+                    />
+                </Link>
+                <div className="flex flex-col space-y-4 max-w-96">
+                    <p className="flex items-center">
+                        <strong>404.&nbsp;</strong>
+                        <span className="text-gray-600">That's an error</span>
+                    </p>
+                    <p>
+                        The requested URL {pathname} was not found on this
+                        server.
+                    </p>
+                </div>
+            </div>
         </DefaultLayout>
     );
 };
