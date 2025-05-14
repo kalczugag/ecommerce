d78c6779dfca55865c89abe12b37d516cb889024
kalczugag <kalczugag@gmail.com>
2025-05-14 12:37:17 +0200

    fix: update Box component background color and set TableContainer background color

--- admin/src/components/Table/index.tsx
+++ admin/src/components/Table/index.tsx
@@ -230,6 +230,7 @@ const CustomPaginationActionsTable = ({
         <Loading isLoading={isLoading}>
             <TableContainer
                 component={Paper}
+                sx={{ bgcolor: "#F5F6FA" }}
                 className="text-text-light dark:text-text-dark"
             >
                 <Table>
